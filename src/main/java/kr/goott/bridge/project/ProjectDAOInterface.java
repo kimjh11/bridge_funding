e792a81124df91ec397028d53181631b6e8ad513
jojae <zmflfm12@gmail.com>
2019-04-12 14:51:31 +0900

    프로젝트 등록 커밋

--- src/main/java/kr/goott/bridge/project/ProjectDAOInterface.java
+++ src/main/java/kr/goott/bridge/project/ProjectDAOInterface.java
@@ -1,6 +1,27 @@
 package kr.goott.bridge.project;
 
+import java.util.List;
 
 public interface ProjectDAOInterface {
 	
+	public List<CategoryVO> selectCate();
+	
+	public int inPro(ProjectVO vo);
+	
+	public ProjectVO selectPro(String userMail);
+	public ProjectVO selectPro2(String userMail,String proCode);
+	public ProjectVO selectPro3(String userMail,int proNum);
+	
+	public int savePro(ProjectVO vo);
+	public int saveStory(ProjectVO vo);
+	public int saveItem(ItemVO vo);
+	public int saveProOk(ProjectVO vo);
+	
+	public List<ItemVO> selectItem(String userMail,String proCode);
+	
+	public int delItem(ItemVO vo);
+	
+	public int changePro(ProjectVO vo);
+	
+	public int delPro3();
 }
