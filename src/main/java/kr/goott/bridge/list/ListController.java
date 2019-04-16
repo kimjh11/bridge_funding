c39889e438a2ae5b4fc6667b0d8634eb90e12bf8
dahye <a4817a@naver.com>
2019-04-16 12:09:51 +0900

    admin 헤더/기본레이아웃

--- src/main/java/kr/goott/bridge/list/ListController.java
+++ src/main/java/kr/goott/bridge/list/ListController.java
@@ -1,19 +1,15 @@
 package kr.goott.bridge.list;
 
-import java.io.File;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.multipart.MultipartHttpServletRequest;
 import org.springframework.web.servlet.ModelAndView;
 
 import kr.goott.bridge.project.CategoryVO;
@@ -94,68 +90,7 @@ public class ListController {
 	}
   
 	
-	String folder;
-	public JdbcTemplate getTemplate() {
-		return template;
-	}
-	public JdbcTemplate template;
 	
-	@Autowired
-	public void setTemplate(JdbcTemplate template) {
-		this.template = template;
-	}
-	
-	@RequestMapping(value="/admin")
-	public String admin() {
-		return "/admin/main";
-	}
-	
-	@RequestMapping(value="/bannerSubmit", method=RequestMethod.POST)
-	public ModelAndView bannerUpload(HttpServletRequest req, BannerVO vo,
-									@RequestParam("bannerImgFile") MultipartFile bannerImgFile) {
-		ModelAndView mav = new ModelAndView();
-		ListDaoInterface dao = sqlSession.getMapper(ListDaoInterface.class);
-		
-		//파일이 저장될 절대경로
-		folder = req.getSession().getServletContext().getRealPath("/upload");
-	
-		String newFileName = null;
-		
-		if(bannerImgFile!=null) {
-			String originName = bannerImgFile.getOriginalFilename();
-			
-			if(originName != null && !originName.equals("")) {
-				//서버에 동일한 파일유무 확인 후 업로드
-				File file = new File(folder, originName);
-				if(file.exists()) {//동일한 파일이 있을경우
-					int cnt=1;
-					while(true) {
-						int dot = originName.lastIndexOf(".");//마지막.위치
-						String preFile = originName.substring(0, dot);//파일명
-						String extFile = originName.substring(dot+1);//확장자
-						File newFile = new File(folder, preFile+"_"+cnt+"."+extFile);
-						if(!newFile.exists()) {
-							newFileName = newFile.getName();
-							break;
-						}
-						cnt++;
-					}
-				}else {//동일한 파일이 없을경우
-					newFileName = originName;
-				}
-				//업로드
-				try {
-					bannerImgFile.transferTo(new File(folder, newFileName));
-				}catch(Exception e) {
-					System.out.println("배너 이미지 파일등록 에러"+e.getMessage());
-				}
-			}
-		}
-		vo.setBannerImg(newFileName);
-		mav.addObject("cnt",dao.bannerInsert(vo));
-		mav.setViewName("/admin/main");
-		return mav;
-	}
 	/*ver)2019-04-10
 	@RequestMapping("/list")
 	public ModelAndView reward(@RequestParam("menuIndex") int menuIndex, 
--- src/main/webapp/WEB-INF/views/admin/main.jsp
+++ src/main/webapp/WEB-INF/views/admin/main.jsp
@@ -8,48 +8,7 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 </head>
 <body>
-<c:if test="${cnt>0 }">
-	<script>  
-		alert('등록완료!');
-	</script>
-</c:if>
-<form action="<%=request.getContextPath() %>/bannerSubmit" method="post" id="bannerSubmitFrm" enctype="multipart/form-data">
-	<ul>
-		<li>
-			<span>페이지</span>
-			<select>
-				<option>선택하세요</option>
-				<option>리워드홈</option>
-				<option>오픈예정</option>
-				<option>기부와후원</option>
-			</select>
-			<input type="hidden" name="pageName" />
-			<script>
-				$(function(){
-					$('#bannerSubmitFrm select').change(function(){
-						$('input[name="pageName"]').val(this.value);
-						console.log(this.value);
-					});
-				});
-			</script>
-		</li>
-		<li>
-			<span>제목</span>
-			<input type="text" name="bannerTitle" />
-		</li>
-		<li>
-			<span>설명</span>
-			<input type="text" name="bannerSubTitle" />
-		</li>
-		<li>
-			<span>이미지</span>
-			<input type="file" name="bannerImgFile" />
-		</li>
-		<li>
-			<input type="submit" value="등록" />
-		</li>
-	</ul>
-</form>
+
 
 </body>
 </html>
